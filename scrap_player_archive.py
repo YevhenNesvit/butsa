56818c832a8f03bdd282a7214395fc88069a1be5
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2025-02-13 22:23:14 +0200

    add clean data func

--- scrap_player_archive.py
+++ scrap_player_archive.py
@@ -1,52 +1,76 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-import time
+import os
+import dotenv
+from bs4 import BeautifulSoup
 
-def get_players_archive_data(age_from, age_to, talent_from, talent_to):
+dotenv.load_dotenv()
 
-    service = Service('/home/yevhen/chromedriver-linux64/chromedriver')
 
-    # Створюємо екземпляр WebDriver з переданим сервісом
+def get_players_row_data(age_from, age_to, talent_from, talent_to, date_from):
+
+    service = Service("/home/yevhen/chromedriver-linux64/chromedriver")
+
     driver = webdriver.Chrome(service=service)
 
-    # Завантаження сторінки
-    driver.get('https://www.butsa.ru/xml/players/transfer.php?page=all&type=players/transfer&act=history')
+    driver.get(
+        "https://www.butsa.ru/xml/players/transfer.php?&type=players/transfer&act=history"
+    )
 
-    username_input = driver.find_element(By.NAME, 'auth_name')  # Замість 'username' вказати правильний атрибут або id
-    password_input = driver.find_element(By.NAME, 'auth_pass')  # Замість 'password' вказати правильний атрибут або id
+    username_input = driver.find_element(By.NAME, "auth_name")
+    password_input = driver.find_element(By.NAME, "auth_pass")
 
-    # Введемо логін і пароль
-    username_input.send_keys('Yevhen')  # Замість 'your_username' вказати ваш логін
-    password_input.send_keys('treize')
+    username_input.send_keys(os.getenv("USERNAME"))
+    password_input.send_keys(os.getenv("PASSWORD"))
 
-    login_button = driver.find_element(By.NAME, 'imageField')  # Замість 'login_button' вказати правильний id або клас
+    login_button = driver.find_element(By.NAME, "imageField")
     login_button.click()
 
-    # Якщо потрібно заповнити перше текстове поле:
-    age_from = driver.find_element(By.NAME, 'Age_1')
-    age_from.send_keys(age_from)
+    age_from_input = driver.find_element(By.NAME, "Age_1")
+    age_from_input.send_keys(age_from)
 
-    # Якщо потрібно заповнити друге текстове поле:
-    age_to = driver.find_element(By.NAME, 'Age_2')  # Замість 'search_input_2_id' вказуєте правильний id
-    age_to.send_keys(age_to)
+    age_to_input = driver.find_element(By.NAME, "Age_2")
+    age_to_input.send_keys(age_to)
 
-    talent_from = driver.find_element(By.NAME, 'Talent_1')  # Замість 'search_input_2_id' вказуєте правильний id
-    talent_from.send_keys(talent_from)
+    talent_from_input = driver.find_element(By.NAME, "Talent_1")
+    talent_from_input.send_keys(talent_from)
 
-    talent_to = driver.find_element(By.NAME, 'Talent_2')  # Замість 'search_input_2_id' вказуєте правильний id
-    talent_to.send_keys(talent_to)
+    talent_to_input = driver.find_element(By.NAME, "Talent_2")
+    talent_to_input.send_keys(talent_to)
 
-    # Знайдемо кнопку фільтрації і натискаємо її
-    button = driver.find_element(By.CLASS_NAME, 'button')
+    date_from_input = driver.find_element(By.NAME, "lottime")
+    date_from_input.send_keys(date_from)
+
+    button = driver.find_element(By.CLASS_NAME, "button")
     button.click()
 
-    # Закриваємо браузер
+    result = driver.page_source
+
     driver.quit()
 
-    return driver.page_source
+    return result
 
-print(get_players_archive_data('16', '16', '4', '4'))
+
+def get_players_clean_data(age_from, age_to, talent_from, talent_to, date_from):
+    row_data = get_players_row_data(age_from, age_to, talent_from, talent_to, date_from)
+    soup = BeautifulSoup(row_data, "html.parser")
+
+    players_data = []
+
+    # Збираємо дані для кожної команди
+    players_td = soup.find("td", id="mainarea_rigth")
+    players_table = players_td.find("table", class_="maintable")
+    players_thead = players_table.find("tbody")
+
+    for row in players_thead.find_all("tr"):
+        tds = row.find_all("td")
+        td_links = [td.find("a")["href"] if td.find("a") else None for td in tds]
+        td_texts = [td.get_text(strip=True) for td in tds]
+
+        players_data.append(td_texts + td_links[0:1])
+
+    return players_data[:-1]
+
+
+print(get_players_clean_data("16", "16", "4", "4", "2024-12-13"))
